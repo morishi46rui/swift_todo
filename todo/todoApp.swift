2bf1c37489bd3bd0b790b263ff1ab6e4fe74ad27
宮森 瑠偉 <r.miyamori@eisansystem.jp>
2024-12-14 16:18:54 +0900

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by USER on 2024/12/14.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
